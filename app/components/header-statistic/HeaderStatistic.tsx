ae9e16daa071af2758380a65b3b2d467ed376b7f
joaovictormendessilva <joao_victor-08@hotmail.com>
2026-06-13 22:59:20 -0300

    fix: fixed header statistic background

--- app/components/header-statistic/HeaderStatistic.tsx
+++ app/components/header-statistic/HeaderStatistic.tsx
@@ -3,13 +3,13 @@ import { HeaderStatisticProps } from "./HeaderStatistic.types";
 export function HeaderStatistic({ title, value, icon, backgroundColor }: HeaderStatisticProps) {
   return (
     <div className="flex gap-[11px] items-center">
-      <div className={`h-[32px] w-[32px] rounded-[12px] bg-[${backgroundColor}] flex items-center justify-center`}>
+      <div className={`h-[32px] w-[32px] rounded-[12px] flex items-center justify-center`} style={{ backgroundColor }}>
         {icon}
       </div>
 
       <div className="flex flex-col">
         <span className="hidden sm:block text-muted-foreground text-[10px]">{title}</span>
-        <span className="hidden sm:block text-foreground text-[14px] font-bold">{value}</span>
+        <span className="hidden sm:block text-foreground text-[14px] font-bold mt-[-4px]">{value}</span>
       </div>
     </div>
   );
